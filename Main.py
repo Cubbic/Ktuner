ba90a26fd4583b53f2893bf162c5c5c34d7b66eb
cubbic <danpopa1998@gmail.com>
2015-09-13 22:21:52 +0300

    Adaugat panel si button

--- Main.py
+++ Main.py
@@ -3,4 +3,11 @@ import wx
 app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
 frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
 frame.Show(True)     # Show the frame.
+
+panel = wx.Panel(frame, id=wx.ID_ANY)
+button = wx.Button(panel,id = wx.ID_ABOUT,label = "About man")
+panel.Show()
+button.Show()
+
+
 app.MainLoop()
