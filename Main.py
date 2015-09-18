631748a86cae13b125c96f5235d7441279d0fb05
cubbic <danpopa1998@gmail.com>
2015-09-18 20:58:20 +0300

    Facut Class MainWindow cu 1 buton care se initializeaza normal

--- Main.py
+++ Main.py
@@ -1,13 +1,18 @@
 import wx
 
-app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
-frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
-frame.Show(True)     # Show the frame.
-
-panel = wx.Panel(frame, id=wx.ID_ANY)
-button = wx.Button(panel,id = wx.ID_ABOUT,label = "About man")
-panel.Show()
-button.Show()
+class MainWindow (wx.Frame):
+    def __init__(self,parent,title):
+        self.frame =  wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
+        self.panel = wx.Panel(self.frame, id=wx.ID_ANY,size = wx.DefaultSize)
+        self.button = wx.Button(self.panel,id = wx.ID_ABOUT,label = "About man" ,pos = (10,10))
+        self.frame.Show(True)
 
 
-app.MainLoop()
+
+
+
+if __name__ == '__main__':
+  
+    app = wx.App()
+    MainWindow(None, title='Size')
+    app.MainLoop()
