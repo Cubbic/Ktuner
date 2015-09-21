e898d1f7f1453b49275ebbe1dcf3689de94812b8
cubbic <danpopa1998@gmail.com>
2015-09-21 21:43:24 +0300

    Facut layoutu si am facut sa mearga din main

--- Main.py
+++ Main.py
@@ -1,18 +1,17 @@
 import wx
+import Layout
+import gettext
 
-class MainWindow (wx.Frame):
-    def __init__(self,parent,title):
-        self.frame =  wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
-        self.panel = wx.Panel(self.frame, id=wx.ID_ANY,size = wx.DefaultSize)
-        self.button = wx.Button(self.panel,id = wx.ID_ABOUT,label = "About man" ,pos = (10,10))
-        self.frame.Show(True)
+ly = Layout
 
 
+    
+if __name__ == "__main__":
+    gettext.install("app") # replace with the appropriate catalog name
 
-
-
-if __name__ == '__main__':
-  
     app = wx.App()
-    MainWindow(None, title='Size')
+    
+    frame = ly.MyFrame(None, wx.ID_ANY, "")
+    app.SetTopWindow(frame)
+    frame.Show()
     app.MainLoop()
