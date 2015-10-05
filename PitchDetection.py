bba2ed3e4f27d3d9fc203675c3d4ff87dba856c6
cubbic <danpopa1998@gmail.com>
2015-10-05 19:02:54 +0300

    it returns input_devices_list

--- PitchDetection.py
+++ PitchDetection.py
@@ -4,14 +4,26 @@ import Layout as ly
 
        
                 
-p = pyaudio.PyAudio()
-info = p.get_host_api_info_by_index(0)
-numdevices = info.get('deviceCount')
-#for each audio device, determine if is an input or an output and add it to the appropriate list and dictionary
-for i in range (0,numdevices):
-        if p.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
-                print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name')
+
 
        
 
-p.terminate()
+
+def get_input_devices ():
+        p = pyaudio.PyAudio()
+        info = p.get_host_api_info_by_index(0)
+        
+        numdevices = info.get('deviceCount')
+        
+        input_devices_list = []
+        #for each audio device, determine if is an input or an output and add it to the appropriate list and dictionary
+        for i in range (0,numdevices):
+                if p.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
+                        #print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name')
+                        input_devices_list.append ( p.get_device_info_by_host_api_device_index(0,i) )
+                     
+          
+        p.terminate()
+        return input_devices_list
+        
+get_input_devices()
