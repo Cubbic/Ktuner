62b4201668d95b60781a9d8dad77e4b547c726ee
cubbic <danpopa1998@gmail.com>
2015-10-31 19:16:30 +0200

    Was added a library for dealing with multiple keys pointing to a single value which i don't why isn't realized in standart dict

--- Main.py
+++ Main.py
@@ -5,21 +5,22 @@ import gettext # for localisation shit required by wxGlade
 import numpy,time ,math
 import alsaaudio 
 import threading , Queue
+from multi_key_dict import multi_key_dict
 
-note_dict = {
-    0,12,24,36,48,60,72,84,96,108,120 :"C",
-    1,13,25,37,49,61,73,85,97,109,121 :"C#",
-    2,14,26,38,50,62,74,86,98,110,122 :"D",
-    3,15,27,39,51,63,75,87,99,111,123 :"D#",
-    4,16,28,40,52,64,76,88,100,112,124:"E",
-    5,17,29,41,53,65,77,89,101,113,125:"F",
-    6,18,30,42,54,66,78,90,102,114,126:"F#",
-    7,19,31,43,55,67,79,91,103,115,127:"G",
-    8,20,32,44,56,68,80,92,104,116    :"G#",
-    9,21,33,45,57,69,81,93,105,117    :"A",
-    10,22,34,46,58,70,82,94,106,118:  :"A#",
-    11,23,35,47,59,71,83,95,107,119:  :"B"
-}
+note_dict = multi_key_dict()
+note_dict[0,12,24,36,48,60,72,84,96,108,120] = "C"
+note_dict[1,13,25,37,49,61,73,85,97,109,121] = "C#"
+note_dict[2,14,26,38,50,62,74,86,98,110,122] = "D"
+note_dict[3,15,27,39,51,63,75,87,99,111,123] = "D#"
+note_dict[4,16,28,40,52,64,76,88,100,112,124]= "E"
+note_dict[5,17,29,41,53,65,77,89,101,113,125]= "F"
+note_dict[6,18,30,42,54,66,78,90,102,114,126]= "F#"
+note_dict[7,19,31,43,55,67,79,91,103,115,127]= "G"
+note_dict[8,20,32,44,56,68,80,92,104,116] =    "G#"
+note_dict[9,21,33,45,57,69,81,93,105,117] =    "A"
+note_dict[10,22,34,46,58,70,82,94,106,118]=    "A#"
+note_dict[11,23,35,47,59,71,83,95,107,119]=    "B"
+
 
 
 def set_input_list():
@@ -68,8 +69,10 @@ def get_midi_note(pitch):
 
 def show_note(pitch):
     midi_note = get_midi_note(pitch)
-    
-
+    try :
+        print note_dict[midi_note]
+    except:
+        print "None Exc"
 
 
 
@@ -100,7 +103,7 @@ if __name__ == "__main__":
     
     app = wx.App()
     frame = ly.MainFrame(None, wx.ID_ANY )
-    #start code inserting
+    #####start code inserting #####
 
     q = Queue.Queue()
     
@@ -109,8 +112,8 @@ if __name__ == "__main__":
     detect_pitch_thread = threading.Thread(target=start_stream , args = (count_note,))
     detect_pitch_thread.start()
 
-    #end code inserting
-    ########app.SetTopWindow(frame)
+    #####end code inserting #####
+    #app.SetTopWindow(frame)
     frame.Show()
     app.MainLoop()
     #after app.MainLoop() no code will be executed
